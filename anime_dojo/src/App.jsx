cf166f34e2c4e18c894dacdc329d3d220e8d0679
Serge-Edouard Joseph <sjoseph.eng@gmail.com>
2022-12-02 08:06:33 -0500

    Server file watch implemented and Data fetch template has been applied
    The applied data fetch is to conduct api pulls within separate docker containers

--- anime_dojo/src/App.jsx
+++ anime_dojo/src/App.jsx
@@ -1,32 +1,63 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState("");
+  const [password,setPassword] = useState("");
+  const [request,setRequest] = useState("");
+  const [response,setResponse] = useState(null)
+
+  let handleChange = function(e) {
+    e.preventDefault;
+
+    let input = e.target
+    if (input.id ==="username") {
+      setUsername(input.value)
+    } else if (input.id === "password") {
+      setPassword(input.value)
+    } else if (input.id === "request") {
+      setRequest(input.value)
+    }
+    console.log(username,password,request)
+  }
+
+  async function apiRequest() {
+    let apiRequest = await fetch("https://www.boredapi.com/api/activity")
+    let data = await apiRequest.json()
+    setResponse(data)
+    addElement(data)
+    console.log("response: ",response)
+  }
+  
+  let sendRequest = function(e) {
+    e.preventDefault;
+    apiRequest()
+    console.log("Submit has been handled")
+  }
+
+  function addElement(data) {
+    // create a new div element
+    const req = document.createElement("div");
+  
+    // and give it some content
+    req.innerHTML = JSON.stringify(data)
+  
+    // add the newly created element and its content into the DOM
+    const lastChild = document.getElementById("container");
+    lastChild.append(req)
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="App" id="container">
+      <form className="credentials" onChange={handleChange}>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" value={username}></input>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" name="password" value={password}/>
+        <label htmlFor="request">Request:</label>
+        <input type="text" id="request" name="request" value={request}/>
+      </form>
+      <button className='request' id="requestButton" onClick={sendRequest}>Send</button>
     </div>
   )
 }
