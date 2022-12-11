910650cbb750a79c2139cd985e066058a57eccb9
Serge-Edouard Joseph <sjoseph.eng@gmail.com>
2022-12-11 14:45:14 -0500

    review#update implemented within the reviews controller

--- user_api/app/controllers/api/reviews_controller.rb
+++ user_api/app/controllers/api/reviews_controller.rb
@@ -18,21 +18,12 @@ class Api::ReviewsController < ApplicationController
         username = find_user
         return if username.nil?
 
-        components = {
-            user: review_params[:user],
-            show: review_params[:show],
-            rating: review_params[:rating].to_i,
-            amount_watched: review_params[:amount_watched],
-            highlighted_points: review_params[:highlighted_points],
-            overall_review: review_params[:overall_review],
-            referral_id: review_params[:referral_id],
-            watch_priority: review_params[:watch_priority]
-        }
+        components = review_attrs
 
         @review = Review.new(components)
 
         if (@review.save) 
-            render :create
+            {status: "complete", review: @review}
         else 
             render json: {status: "failed", errors: @review.errors.objects.first.full_message}
         end
@@ -41,18 +32,29 @@ class Api::ReviewsController < ApplicationController
 
     def show
         # get the specific review of a specific user
-        username = find_user
-        return if username.nil?
+        review = find_show
+        return if review.nil?
 
-        show = review_params[:id]
-        review = Review.where(["reviews.user = ? and show = ?",username,show]).take
+        render json: {status: "complete", review: review}
+    end
 
-        if !review
-            render json: {status: "failed", error: "No review for this show"}
-            return
+    def update
+        review = find_show
+        return if review.nil?
+
+        components = review_attrs(review)
+        review.update(components)
+
+        if (review.save) 
+            render json: {status: "complete", review: review}
+        else 
+            render json: {status: "failed", errors: review.errors.objects.first.full_message}
         end
+    end
 
-        render json: {status: "complete", reviews: review}
+    def destroy
+        review = find_show(username)
+        return if review.nil?
     end
 
     #helper functions
@@ -70,5 +72,41 @@ class Api::ReviewsController < ApplicationController
         end
 
         user.username
+    end
+
+    def find_show
+        username = find_user
+        return nil if username.nil?
+
+        show = review_params[:id] || review_params[:show]
+        review = Review.where(["reviews.user = ? and show = ?",username,show]).take
+
+        if !review
+            render json: {status: "failed", error: "No review for this show"}
+        end
+
+        review
+    end
+
+    def review_attrs(review = nil)
+        user = review_params[:user_id] || review_params[:user]
+        show = review_params[:id] || review_params[:show]
+        rating = review_params[:rating] || review&.rating
+        amount_watched = review_params[:amount_watched] || review&.amount_watched
+        hps = review_params[:highlighted_points] || review&.highlighted_points
+        overall_review = review_params[:overall_review] || review&.overall_review
+        referral_id = review_params[:referral_id] || review&.referral_id
+        wp = review_params[:watch_priority] || review&.watch_priority
+
+        components = {
+            user: user,
+            show: show,
+            rating: rating.to_i,
+            amount_watched: amount_watched,
+            highlighted_points: hps,
+            overall_review: overall_review,
+            referral_id: referral_id,
+            watch_priority: wp.to_i
+        }
     end
 end
