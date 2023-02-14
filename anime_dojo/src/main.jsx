fdfb0d55132e29937c30c2334446c695363a0672
Serge-Edouard Joseph <sjoseph.eng@gmail.com>
2023-02-14 17:33:59 -0500

    SSE testing interface has begun construction

--- anime_dojo/src/main.jsx
+++ anime_dojo/src/main.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import UserTable from './testing/user_api/UserTable'
-import ReviewTable from './testing/user_api/ReviewsTable'
-import ReviewCommentsTable from './testing/user_api/ReviewsCommentsTable'
-import RoomTable from './testing/user_api/RoomTable'
-import ForumsTable from './testing/user_api/ForumsTable'
-import ForumCommentsTable from './testing/user_api/ForumCommentsTable'
-import ShowRatingsTable from './testing/show_rating_api/ShowRatingsTable'
-import NotificationsTable from './testing/notification_api/notificationsTable'
-import WatchLaterTable from './testing/user_api/watchLaterTable'
-
-import './index.css'
+// import UserTable from './testing/user_api/UserTable'
+// import ReviewTable from './testing/user_api/ReviewsTable'
+// import ReviewCommentsTable from './testing/user_api/ReviewsCommentsTable'
+// import RoomTable from './testing/user_api/RoomTable'
+// import ForumsTable from './testing/user_api/ForumsTable'
+// import ForumCommentsTable from './testing/user_api/ForumCommentsTable'
+// import ShowRatingsTable from './testing/show_rating_api/ShowRatingsTable'
+// import NotificationsTable from './testing/notification_api/notificationsTable'
+// import WatchLaterTable from './testing/user_api/watchLaterTable'
+import Interface from './testing/SSE/interface'
+// import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ReviewTable />
+    <Interface />
   </React.StrictMode>
 )
---
+++ anime_dojo/src/testing/SSE/interface.jsx
@@ -0,0 +1,41 @@
+import "./SSE CSS/main.css"
+import { useState } from "react"
+
+export default function Interface() {
+    const [username, setUsername] = useState("");
+    const [actionUser, setActionUser] = useState("");
+
+    return(
+        <div className="interface">
+            <UserSelect userType="user" username={username} setUsername={setUsername} />
+            <div className="userData">
+                <div className="box" id="one"></div>
+                <div className="box" id="two"></div>
+                <div className="box" id="three"></div>
+                <div className="box" id="four"></div>
+                <div className="box" id="five">
+                    <div className="accordian" id="first"></div>
+                    <div className="accordian" id="second"></div>
+                    <div className="accordian" id="third"></div>
+                </div>
+            </div>
+            <UserSelect userType="actionUser" username={actionUser} setUsername={setActionUser} />
+            <div className="actionUserAction"></div>
+            
+        </div>
+    )
+}
+
+function UserSelect(props) {
+    const userType = props.userType;
+    const setUsername = props.setUsername;
+
+    const userID = userType == "user" ? "User" : "ActionUser";
+
+    return(
+        <div className="userEntry" id={userID}>
+            <p>{userID}: </p>
+            <input className="userInputField"></input>
+        </div>
+    )
+}
